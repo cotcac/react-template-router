2708ee40423d9a8e4a57abaa3d4ed9039a19328c
Kinny Tran <kinny.tran@contemi.com.vn>
2019-06-14 14:11:20 +0700

    error boundary

--- src/App.js
+++ src/App.js
@@ -4,26 +4,29 @@ import Footer from './template/Footer';
 import Nav from './template/Nav';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
 const Home = lazy(() => import('./routes/Home'));
 const About = lazy(() => import('./routes/About'));
 function App() {
   return (
     <Router>
-      <Suspense fallback={<div>Loading...</div>}>
-        <div className="App">
-          <Header />
-          <Nav></Nav>
-          <div className="container">
-            <div className="row">
-              <Switch>
-                <Route path="/" exact component={Home}></Route>
-                <Route path="/about" component={About}></Route>
-              </Switch>
+      <ErrorBoundary>
+        <Suspense fallback={<div>Loading...</div>}>
+          <div className="App">
+            <Header />
+            <Nav></Nav>
+            <div className="container">
+              <div className="row">
+                <Switch>
+                  <Route path="/" exact component={Home}></Route>
+                  <Route path="/about" component={About}></Route>
+                </Switch>
+              </div>
             </div>
+            <Footer />
           </div>
-          <Footer />
-        </div>
-      </Suspense>
+        </Suspense>
+      </ErrorBoundary>
     </Router>
   );
 }
