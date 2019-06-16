85b7caea078a0e53f04aa26fb88749e57b0216a7
Your Name <you@example.com>
2019-06-16 14:33:56 +0700

    fix reload whole page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React, { Suspense, lazy } from 'react';
-import Footer from './template/Footer';
-import Nav from './template/Nav';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Footer from './template/Footer';
+import Nav from './template/Nav';
 import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
 import NotFound from './components/NotFound/NotFound';
 
-const Users = lazy(()=> import('./routes/Users'))
-const Posts = lazy(()=> import('./routes/Posts'))
-const UserDefail = lazy(()=> import('./routes/UserDefail'));
-const UserInsert = lazy(()=> import('./routes/UserInsert'));
+const Users = lazy(() => import('./routes/Users'))
+const Posts = lazy(() => import('./routes/Posts'))
+const UserDefail = lazy(() => import('./routes/UserDefail'));
+const UserInsert = lazy(() => import('./routes/UserInsert'));
 const Home = lazy(() => import('./routes/Home'));
 const About = lazy(() => import('./routes/About'));
 function App() {
   return (
     <Router>
-      <ErrorBoundary>
-        <Suspense fallback={<div>Loading...</div>}>
-          <div className="App">
-            <Nav></Nav>
-            <div className="container">
-              <div className="row">
+
+      <div className="App">
+        <Nav></Nav>
+        <div className="container">
+          <div className="row">
+            <ErrorBoundary>
+              <Suspense fallback={<div>Loading...</div>}>
                 <Switch>
                   <Route path="/" exact component={Home}></Route>
                   <Route path="/about" component={About}></Route>
@@ -28,15 +29,15 @@ function App() {
                   <Route path="/users/:id" exact component={UserDefail}></Route>
                   <Route path="/users-insert" exact component={UserInsert}></Route>
                   <Route path="/posts" exact component={Posts}></Route>
-
                   <Route component={NotFound}></Route>
                 </Switch>
-              </div>
-            </div>
-            <Footer />
+              </Suspense>
+            </ErrorBoundary>
           </div>
-        </Suspense>
-      </ErrorBoundary>
+        </div>
+        <Footer />
+      </div>
+
     </Router>
   );
 }
