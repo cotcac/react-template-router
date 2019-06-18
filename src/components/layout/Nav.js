1714c8b87b859cf2f7a5d8f899170ed72621015c
Kinny Tran <kinny.tran@contemi.com.vn>
2019-06-18 17:55:09 +0700

    nav collap

--- src/components/layout/Nav.js
+++ src/components/layout/Nav.js
@@ -3,12 +3,17 @@ import { NavLink } from 'react-router-dom';
 import './Nav.css';
 class Nav extends Component {
     render() {
+        const clickMe =()=>{
+            console.log('heee');
+          
+            
+        }
         return (
             <nav className="navbar navbar-reverse navbar-fixed-top">
                 <div className="container">
                     <div className="row">
                         <div className="navbar-header">
-                            <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#myNavbar">
                                 <span className="icon-bar"></span>
                                 <span className="icon-bar"></span>
                                 <span className="icon-bar"></span>
@@ -17,9 +22,9 @@ class Nav extends Component {
                         </div>
                         <div className="collapse navbar-collapse" id="myNavbar">
                         <ul className="nav navbar-nav">
-                            <li> <NavLink activeClassName="current" to="/about">About</NavLink> </li>
-                            <li> <NavLink activeClassName="current" to="/users">Users</NavLink> </li>
-                            <li> <NavLink activeClassName="current" to="/posts">Posts</NavLink> </li>
+                            <li> <NavLink activeClassName="current" data-toggle="collapse" data-target="#myNavbar" onClick={clickMe} to="/about">About</NavLink> </li>
+                            <li> <NavLink activeClassName="current" data-toggle="collapse" data-target="#myNavbar" to="/users">Users</NavLink> </li>
+                            <li> <NavLink activeClassName="current" data-toggle="collapse" data-target="#myNavbar" to="/posts">Posts</NavLink> </li>
                         </ul>
                         <ul className="nav navbar-nav navbar-right">
                             <li><NavLink to="/sign-up"><span className="glyphicon glyphicon-user"></span> Sign Up</NavLink></li>
