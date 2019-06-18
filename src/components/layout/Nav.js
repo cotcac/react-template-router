f8237e2e5f6c158f1c32a8b5fb54225512a4b30b
Kinny Tran <kinny.tran@contemi.com.vn>
2019-06-18 16:29:51 +0700

    nav update

--- src/components/layout/Nav.js
+++ src/components/layout/Nav.js
@@ -8,17 +8,24 @@ class Nav extends Component {
                 <div className="container">
                     <div className="row">
                         <div className="navbar-header">
+                            <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                            </button>
                             <NavLink className="navbar-brand" to="/">CONTEMI</NavLink>
                         </div>
+                        <div className="collapse navbar-collapse" id="myNavbar">
                         <ul className="nav navbar-nav">
                             <li> <NavLink activeClassName="current" to="/about">About</NavLink> </li>
                             <li> <NavLink activeClassName="current" to="/users">Users</NavLink> </li>
                             <li> <NavLink activeClassName="current" to="/posts">Posts</NavLink> </li>
                         </ul>
-                        <ul class="nav navbar-nav navbar-right">
-                            <li><NavLink to="/sign-up"><span class="glyphicon glyphicon-user"></span> Sign Up</NavLink></li>
-                            <li><NavLink to="/login"><span class="glyphicon glyphicon-log-in"></span> Login</NavLink></li>
+                        <ul className="nav navbar-nav navbar-right">
+                            <li><NavLink to="/sign-up"><span className="glyphicon glyphicon-user"></span> Sign Up</NavLink></li>
+                            <li><NavLink to="/login"><span className="glyphicon glyphicon-log-in"></span> Login</NavLink></li>
                         </ul>
+                        </div>
                     </div>
                 </div>
             </nav>
---
+++ src/routes/readme.md
@@ -0,0 +1,1 @@
+# this is the container of many components. 
